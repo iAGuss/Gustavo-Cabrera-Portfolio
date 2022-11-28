0c27e5ec9fca0518a2108f4463845a2e6a6c2eea
iAGuss <gustavodario2001@gmail.com>
2022-11-28 20:08:33 -0300

    subiengo.gh-pages

--- src/components/Poyectos/Proyectos.jsx
+++ src/components/Poyectos/Proyectos.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import Aos from "aos";
 import "./proyectos.css";
 import Cartap from "../cartaproyectos/Cartap";
 import listaproyectos from "../../constantes/listaproyectos";
+import "aos/dist/aos.css";
 function Proyectos() {
+  Aos.init({ duration: 2000 });
   return (
     <section className="Padre-proyectos">
       <div className="Tipo-de-proyectos">
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-
 import "./main.css";
-
 import foto from "../../assets/IMG_20220809_164244 (2)-min.jpg";
-import GitHub from "../../assets/30b150cd489202db131009ac9540cec0.png";
 import Cv from "../../assets/CV-Gus Full Stack Web Developer.pdf";
 import Linkedin from "../../assets/linkedin.png";
 
@@ -11,7 +8,7 @@ function Main() {
   return (
     <main className="padre-main">
       <div className="texto-precentacion">
-        <p className="Mi-nombre">Hola, soy Gus</p>
+        <p className="Mi-nombre">Hi, I'm Gustavo</p>
 
         <span className="skills">
           <h2 className="Full-Stack" style={{ fontSize: "30px" }}>
@@ -23,9 +20,9 @@ function Main() {
         </span>
         <div className="about-me">
           <p>
-            Mis tecnologías principales son ReactJS, JavaScript y Nodejs además
-            de HTML/CSS cuento con mas de 1 año de experiencia en proyectos
-            personales.
+            Mis tecnologías principales son ReactJS, JavaScript , Nodejs y
+            PostgreSQL además de HTML/CSS cuento con mas de 1 año de experiencia
+            en proyectos personales.
           </p>
         </div>
       </div>
@@ -38,7 +35,7 @@ function Main() {
               Descarga CV
             </a>
           </button>
-         
+
           <a
             href="https://www.linkedin.com/in/gusc/"
             target="_blank"
