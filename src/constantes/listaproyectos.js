f8ea209242229a3b12fd059445e5ab9bf0f84eaa
Gustavo <gustavodario2001@gmail.com>
2024-09-11 11:01:23 -0300

    imagenes

--- src/constantes/listaproyectos.js
+++ src/constantes/listaproyectos.js
@@ -2,7 +2,7 @@ const proyectos = [
   {
     name: "Pokedex con base de datos",
     imagen:
-      "https://swpn4q.sn.files.1drv.com/y4mAwyWabmFJPVH0SgfxPX7uiHtsuGpAmDllgGnFzZl9VXEqZcNLloVmWgtv-TAazBfi9iyyg8Q2wLbwOdwwiLWdFeAhiepGbh9DXq93Ccn57P_BuH9DyHjK3GzBvXvhGn2oa_2-yJWUTqA1ulrT9rL7s1g1DHRwoMK7yYZKuIY60Za8u8XCEXrLVpk2HvCNsvLr5G_xLMdeMFSrEeQUjSTTcF3OoCUW79VO9FLWFh2L3Y",
+      "https://1drv.ms/i/s!Aqt7qbJii_4_gZmePjdbcx-8xddU6_k?embed=1&width=1904&height=925",
 
     postgreSQL:
       "https://cdn.iconscout.com/icon/free/png-256/postgresql-11-1175122.png",
@@ -15,7 +15,7 @@ const proyectos = [
   {
     name: "Pagina Nexos",
     imagen:
-      "https://ekyt7g.sn.files.1drv.com/y4mthx__gj7z7OD1oPEEyUqWKxwMIKIrOmYZtnQMLV906_QZ_CSFKwNvb0XLIZ-uHsFt5_w5f_lhhfylRd2gWkjIDamwlDL7MqAnNYRjTzFVR7tFf_GxdjmNtpCgeeFeqrhjAJlSl8qyOhLc6E7R9yb-A64ktsLdB44uUrsS4ISeWBmeQi4AOE0SGLEBWpYzojz7AHG9s2UztZdX7zo5SoftJ6cgCps09P9dPB_GAV7Wsc",
+      "https://1drv.ms/i/s!Aqt7qbJii_4_gZjdYVTSGeQW-6vuFYs?embed=1&width=1920&height=933",
 
     descripcion: "Maquetacion para la empresa Nexos ",
     GitHub: "https://github.com/iAGuss/Projecto-html_css_js-pasado-a-react",
@@ -24,7 +24,7 @@ const proyectos = [
   {
     name: "Lista de tareas",
     imagen:
-      "https://3tedra.sn.files.1drv.com/y4ml9d1pWOZM6xG1Xgaur49deaYGx2WpdEbGixDjzyTUXJqGKOV7YyJ3iZ5C5W9qQVLTBY7lRac2X1owYU2HyWrq9t7lAIFIxO8rO0mRS6N0Yki4XR6Du8fMUjxTPsmG7In547SYJfDX8j99BnfCBgOuO6WlQRGKWO_iTWwlGocPeSCP5Ebn2YS-jM-51t23zvDF5CZEaJpXBa6HYxjWXK4ZOV1sSj6AIHdU5yXYcI86Fc",
+      "https://1drv.ms/i/s!Aqt7qbJii_4_gZjbUR3YAwPEYEzRQc4?embed=1&width=1915&height=934",
 
     descripcion:
       "Creacion de lista de tareas segun que importancia debas hacer tu tarea ademas de poder borrar las tareas ya completadas",
@@ -34,7 +34,7 @@ const proyectos = [
   {
     name: "Rick and Morty",
     imagen:
-      "https://3tdnww.sn.files.1drv.com/y4m-WusN5gsHgYogY4ZsU-U1Zrltp9kupmhRsdgjl4CSjfkKJ57CLXsqgSsEV0I7KcE1YZGQyvv3PeImkYRmI7j94F1JYVVeRnWVfv4wZdfsmT9DtREhAvO7oFky7IvBBp5PfJpv6Wd0ucoOzsHYEe0JYBrSqyn8bR0TMo6r3aBJ8rTzwNMpmd0qtdK4skhysU0MlD2N0g1Jvjyuk9mIfnf5EcttQqFWZYdAHVUbhqOLNg",
+      "https://1drv.ms/i/s!Aqt7qbJii_4_gZjbTwqeNnKHX6zBESc?embed=1&width=1882&height=933",
     descripcion:
       "Consumó de api  de Rick and Morty ademas de uso de Paginacion y Boostrap",
     GitHub: "https://iaguss.github.io/Api-Rick-and-Morty/",
